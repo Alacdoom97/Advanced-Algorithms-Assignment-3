c531c815d4a95d1e55a34964e3699b6ae1befab7
Pranav <pranav.dilip.patel@gmail.com>
2018-11-05 22:44:58 +0100

    start

---
+++ src/assignment3/MyDirectedGraph.java
@@ -0,0 +1,45 @@
+package assignment3;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyDirectedGraph implements A3Graph {
+
+	List<Integer> gList = new LinkedList<Integer>();
+	int nrOfVert = 0;
+
+	@Override
+    public void addVertex(int vertex) {
+		if (nrOfVert == 0) {
+    	gList.add(vertex);
+		}
+
+    }
+
+	@Override
+	public void addEdge(int sourceVertex, int targetVertex) {
+		if (nrOfVert <= 1) {
+			System.err.println("Error! There aren't enough vertices for there to be an edge");
+		}
+
+	}
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAcyclic() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<List<Integer>> connectedComponents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
